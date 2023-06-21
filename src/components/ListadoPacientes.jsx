c004bd5e497dcdede7957888d5316e26aa721515
sergiolara <sergiolaradiaz3@gmail.com>
2023-06-21 14:20:00 -0500

    sinLocal

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,36 @@
-const ListadoPacientes = () => {
+import Paciente from "./Paciente"
+
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente }) => {
+
+
   return (
-    <div className="w-1/2 lg:w-3/5">
-      <h2>ListadoPacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+
+          {pacientes.map( paciente => (
+            <Paciente
+             key={paciente.id}
+             paciente={paciente}
+             setPaciente={setPaciente} 
+             eliminarPaciente={eliminarPaciente}
+             />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+           Comienza agregando pacientes{""}
+            <span className="text-indigo-600 font-bold"> y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   );
 }
